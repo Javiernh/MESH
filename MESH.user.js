0abb4bbaa0f193348467d0ea31a2dc8b3e09de59
Javier <games.javi@gmail.com>
2016-04-28 00:44:41 +0200

    0.5b

--- MESH.user.js
+++ MESH.user.js
@@ -12,7 +12,7 @@
 // @resource	translation:es https://raw.githubusercontent.com/Javiernh/MESH/beta/translate/locales/es/translation.json
 // @resource	translation:en https://raw.githubusercontent.com/Javiernh/MESH/beta/translate/locales/en/translation.json
 // @resource	translation:fr https://raw.githubusercontent.com/Javiernh/MESH/beta/translate/locales/fr/translation.json
-// @version	0.4.1b
+// @version	0.5b
 // ==/UserScript==
 /* jshint -W043 */
 
@@ -82,58 +82,32 @@ MESH.MakeInput = function(content, src, imgheight, tiptitle, tipdesc) {
 };	// END FUNCTION - MESH.MakeInput
 
 MESH.initWindow = function() {
-	// ----------------- WINDOW ----------------- //
-	var win = $('<div>')
-		.attr('id', 'MESH')
-		.attr('tabindex', '-1')
-		.addClass('ui-dialog ui-widget ui-widget-content')
-		.css({"z-index": "8", "height": "auto", "width": "755px", "top": "-10px", "left": "60px", "display": "block"})
-		.appendTo('#cdUiHook');
-	// ----------------- TITLE ----------------- //
-	var wintitle = $('<div></div>::before')
-		.addClass('ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix')
-		.appendTo(win);
-	$('<span>' + MESH.Title + '</span>')
-		.addClass('ui-dialog-title MESH-title')
-		.appendTo(wintitle);
-	$('<span>(v' + MESH.version + ')</span>')
-		.addClass('ui-dialog-title MESH-subtitle')
-		.appendTo(wintitle);
-	// ----------------- CLOSE (x) ----------------- //
-	var wina = $('<a href="#" role="button"></a>::after')
-		.addClass('ui-dialog-titlebar-close ui-corner-all')
-		.attr('onclick', '$("#MESH").hide()')
-		.appendTo(wintitle);
-	$('<span>close</span>')
-		.addClass('ui-icon ui-icon-closethick')
-		.appendTo(wina);
-	// ----------------- DRAGGABLE ----------------- //
-	win.draggable({ handle: ".ui-dialog-titlebar" });
 	// ----------------- CONTENT ----------------- //
-	var divform = $('<div>').appendTo(win);
+	var divform = $('<div>')
+		.attr('id', 'MESH')
+		.attr('title', MESH.Title)
+		.appendTo('#cdUiHook');
 	var form = $('<form>').attr('id', 'MESH-form').appendTo(divform);
 	var planet = $('<div>').addClass('MESH-planet MESHfloat').appendTo(form);
 	var astrotags = $('<div>').addClass('MESH-astrotags MESHfloat').appendTo(form);
 	var exploheroes = $('<div>').addClass('MESH-exploheroes MESHfloat').appendTo(form);
 	var output = $('<div>').addClass('MESH-output MESHfloat').appendTo(form);
 	MESH.astroinfo(planet, astrotags);
+	MESH.index = (!$('#cdBottomBlock li[data-p="_ICARUS_LARGER_BAY"]').length) ? 4 : 6;
 	MESH.exploHeroes(exploheroes);
 	MESH.generatext(output);
+	$('#MESH').dialog({
+		autoOpen: false,
+		width: 787,
+		resizable: false,
+//		zIndex: 8
+		});
 	// ----------------- SELECT INPUT ON CLICK ----------------- //
 	$('#MESH-form input, #MESH-form textarea').on('click', function() {
 		$(this).focus();
 		$(this).select();
 	});
 };	// END FUNCTION - MESH.initWindow
-
-MESH.OpenWindow = function() {
-	if ($('#cdBottomBlock li[data-p="_ICARUS_LARGER_BAY"]').length === 0) { MESH.index = 4; }	// Max. 4 heroes in exploration.
-	else { MESH.index = 6; }	// Max. 6 heroes in exploration.
-
-	var $MESHwindow = $('#MESH');
-	if ($MESHwindow.length === 0) { MESH.initWindow(); }
-	else { $MESHwindow.show(); }
-};	// END FUNCTION - MESH.OpenWindow
 
 MESH.astroinfo = function(pdata, astrotags) {
 	// ----------------- PLANET DATA ----------------- //
@@ -175,46 +149,6 @@ MESH.herobag = function(divhero, pos) {
 			group++;
 		}
 	}
-};	// END FUNCTION - MESH.herobag
-
-MESH.heroskills = function(addTo, pos) {
-	var divskills = $('<div>').addClass('MESH-skills MESHfloat').appendTo(addTo);
-	var skill = 0;
-	for (var index in MESH.skills) {
-		skill = MESH.skills[index];
-		MESH.MakeInput($('<input type="checkbox" name="' + skill + pos + '" value="' + skill + '">'),
-			'/img/icons/skills/' + skill + '.png', 21, i18next.t(skill + '.title'), i18next.t(skill + '.desc')).appendTo(divskills);
-	}
-};	// END FUNCTION - MESH.heroskills
-
-MESH.exploHeroes = function(addTo) {
-//	var ulHeroes = $('<ul>').addClass('MESH-heroes').appendTo(addTo);
-	for (var i = 0; i < MESH.index; i++) {
-//		var liHeroes = $('<li>').addClass('MESH-heroes').appendTo(ulHeroes);
-		var liHeroes = $('<div>').addClass('MESH-heroes').appendTo(addTo);
-		var divhero = $('<div>').addClass('MESH-heroname MESHfloat').appendTo(liHeroes);
-		$('<span>' + i18next.t('heroe', { num: i+1 }) + ' </span>').addClass('MESHfloat').appendTo(divhero);
-		var select = $('<select name="hero' + i + '">').addClass('MESHfloat').appendTo(divhero);
-		$('<option value="" selected>-----</option>').appendTo(select);
-		for (var hero in MESH.heroes) {
-			$('<option value="' + MESH.heroes[hero] + '">' + MESH.heroes[hero].replace("_", " ").capitalize() + '</option>').appendTo(select);
-		}
-		$('<span> ' + i18next.t('bag') + ' </span>').addClass('MESHfloat').appendTo(liHeroes);
-		MESH.herobag(liHeroes, i);
-		MESH.heroskills(divhero, i);
-	}
-
-	// Hide / show heroes selected / deselected
-	var prevHero = [];
-	$('.MESH-exploheroes select[name^="hero"]').change(function() {
-		var indsel = parseInt($(this).attr('name').replace('hero', ''));
-		var hideHero = $(this).val();
-		if (hideHero !== "") {
-			$('.MESH-exploheroes option[value=' + hideHero + ']').hide();
-		}
-		$('.MESH-exploheroes option[value=' + prevHero[indsel] + ']').show();
-		prevHero[indsel] = $(this).val();
-	});
 	// Add / remove "recommended" class to items
 	$('.MESH-astrotags input, input[name^="diplomacy"').change(function() {
 //		var zone = $(this).attr('name');
@@ -306,6 +240,45 @@ MESH.exploHeroes = function(addTo) {
 		}
 		else { $('input[value="survival"]').parent().removeClass('recommended'); }
 	});
+
+};	// END FUNCTION - MESH.herobag
+
+MESH.heroskills = function(addTo, pos) {
+	var divskills = $('<div>').addClass('MESH-skills MESHfloat').appendTo(addTo);
+	var skill = 0;
+	for (var index in MESH.skills) {
+		skill = MESH.skills[index];
+		MESH.MakeInput($('<input type="checkbox" name="' + skill + pos + '" value="' + skill + '">'),
+			'/img/icons/skills/' + skill + '.png', 21, i18next.t(skill + '.title'), i18next.t(skill + '.desc')).appendTo(divskills);
+	}
+};	// END FUNCTION - MESH.heroskills
+
+MESH.exploHeroes = function(addTo) {
+	for (var i = 0; i < MESH.index; i++) {
+		var liHeroes = $('<div>').addClass('MESH-heroes').appendTo(addTo);
+		var divhero = $('<div>').addClass('MESH-heroname MESHfloat').appendTo(liHeroes);
+		$('<span>' + i18next.t('heroe', { num: i+1 }) + ' </span>').addClass('MESHfloat').appendTo(divhero);
+		var select = $('<select name="hero' + i + '">').addClass('MESHfloat').appendTo(divhero);
+		$('<option value="" selected>-----</option>').appendTo(select);
+		for (var hero in MESH.heroes) {
+			$('<option value="' + MESH.heroes[hero] + '">' + MESH.heroes[hero].replace("_", " ").capitalize() + '</option>').appendTo(select);
+		}
+		$('<span> ' + i18next.t('bag') + ' </span>').addClass('MESHfloat').appendTo(liHeroes);
+		MESH.herobag(liHeroes, i);
+		MESH.heroskills(divhero, i);
+	}
+
+	// Hide / show heroes selected / deselected
+	var prevHero = [];
+	$('.MESH-exploheroes select[name^="hero"]').change(function() {
+		var indsel = parseInt($(this).attr('name').replace('hero', ''));
+		var hideHero = $(this).val();
+		if (hideHero !== "") {
+			$('.MESH-exploheroes option[value=' + hideHero + ']').hide();
+		}
+		$('.MESH-exploheroes option[value=' + prevHero[indsel] + ']').show();
+		prevHero[indsel] = $(this).val();
+	});
 };	// END FUNCTION - MESH.exploHeroes
 
 MESH.varsinit = function() {
@@ -318,6 +291,7 @@ MESH.varsinit = function() {
 	MESH.artefact = 0;
 	MESH.death = 0;
 	MESH.fatigue = 0;
+	MESH.strength = 0;
 	MESH.fight = [10,12,32,12,8,18,15,15];
 	MESH.fightzones = 0;
 	MESH.groupdeath = 0;
@@ -340,37 +314,45 @@ MESH.varsinit = function() {
 	MESH.sprint = 0;
 	MESH.survival = 0;
 	MESH.skilful = 0;	// TODO: Need to include in html (polyvalent)
-
-	var isBlaster = '';
-	var ifblaster = '';
 };	// END FUNCTION - MESH.varsinit
 
 MESH.calcs = function() {
-	var ifight = 0;
-	var isteaks = 0;
+	var ifight = 0, isteaks = 0, isDriller = 1;
 	MESH.varsinit();
 	MESH.pname = $('[name="pname"]').val().capitalize();
 	MESH.pfuel = $('[name="pfuel"]').val();
 	MESH.pdirection = i18next.t('direction.' + $('[name="pdirection"]').val());
-
 	$('.MESH-heroes').each(function(index) {
+		var $herosel = $(this).find('select[name^="hero"]');
+		if ($herosel.val() === "") { return true; }
+		MESH.strength++;
 		if ( $('input[name="botanic' + index + '"]').is(':checked') ) { MESH.botanic += 1; }
 		if ( $('input[name="diplomacy' + index + '"]').is(':checked') ) { MESH.diplomacy = 1; }
-		if ( $('input[name="gunman' + index + '"]').is(':checked') ) {
-			for (var i = 0; i < 4; i++) {
-				ifblaster = $('li select[name$="' + i + 'hero' + index + '"]').val();
-				isBlaster = (ifblaster == 'Blaster') ? ifblaster : isBlaster;
-			}
-			if ( isBlaster == 'Blaster' ) { MESH.gunman += 1; }
+		// Team strength for wearing weapons and gunman skill
+		for (var i = 0; i < 4; i++) {
+			var item = $('li select[name="item' + i + 'hero' + index + '"]').val();
+			MESH.strength += (item == 'blaster') ? ( $('input[name="gunman' + index + '"]').is(':checked') ) ? 2 : 1 : 0;
+			MESH.strength += (item == 'knife') ? 1 : 0;
+			MESH.strength += (item == 'natamy_riffle') ? ( $('input[name="gunman' + index + '"]').is(':checked') ) ? 2 : 1 : 0;
+			MESH.strength += (item == 'items_decoup_05') ? 1 : 0;
+			MESH.strength += (item == 'machine_gun') ? 2 : 0;
+			MESH.strength += (item == 'missile_launcher') ? 3 : 0;
+			MESH.strength += (item == 'grenade') ? 3 : 0;
+			isDriller = (item == 'driller') ? 2 : isDriller;	console.log(isDriller);
 		}
 		if ( $('input[name="sprint' + index + '"]').is(':checked') ) { MESH.sprint += 1; }
 		if ( $('input[name="survival' + index + '"]').is(':checked') ) { MESH.survival += 1; }
 		// TODO: Need to add "Polyvalent" checkbox
 		if ( $('input[name="skilful' + index + '"]').is(':checked') ) { MESH.skilful += 1; }
+
+		MESH.group.push($herosel.val().replace('_', ' ').capitalize());
+		MESH.bag0.push($(this).find('select[name^="item0"]').val());
+		MESH.bag1.push($(this).find('select[name^="item1"]').val());
+		MESH.bag2.push($(this).find('select[name^="item2"]').val());
+
 	});
-	var zone = 0;
 	for (var index in MESH.astroTags) {
-		zone = MESH.astroTags[index];
+		var zone = MESH.astroTags[index];
 		var zonval = parseInt($('.MESH-astrotags input[name="' + zone + '"]').val());
 		MESH.Totzones += zonval;
 		if (zonval !== 0) {
@@ -382,10 +364,10 @@ MESH.calcs = function() {
 		}
 		// Fuel
 		if (zone == 'hydrocarbon' || zone == 'cave' || zone == 'mountain' || zone == 'wreck') {
-			if (zone == 'hydrocarbon') { MESH.maxfuel += 6*zonval; }
-			else if (zone == 'cave') { MESH.maxfuel += 2*zonval; }
-			else if (zone == 'mountain') { MESH.maxfuel += zonval; }
-			else if (zone == 'wreck') { MESH.maxfuel += 3*zonval; }
+			if (zone == 'hydrocarbon') { MESH.maxfuel += 6 * zonval * isDriller; }
+			else if (zone == 'cave') { MESH.maxfuel += 2 * zonval * isDriller; }
+			else if (zone == 'mountain') { MESH.maxfuel += zonval * isDriller; }
+			else if (zone == 'wreck') { MESH.maxfuel += 3 * zonval * isDriller; }
 		}
 		// Alien Artefact
 		if (zone == 'intelligent' || zone == 'mankarog' || zone == 'cave' || zone == 'ruins' || zone == 'wreck') {
@@ -397,15 +379,15 @@ MESH.calcs = function() {
 		}
 		// Provision
 		if (zone == 'insect' || zone == 'intelligent' || zone == 'predator' || zone == 'ruminant' || zone == 'ocean') {
-			if (zone == 'ocean') { MESH.maxsteaks += zonval*(3+MESH.survival); }
-			else { MESH.maxsteaks += (isteaks+1+MESH.survival)*zonval; }
+			if (zone == 'ocean') { MESH.maxsteaks += zonval * (3 + MESH.survival); }
+			else { MESH.maxsteaks += (isteaks + 1 + MESH.survival) * zonval; }
 			isteaks++;
 		}
 		// Harvest
 		if (zone == 'hot' || zone == 'forest' || zone == 'mountain' || zone == 'fruit_trees' || zone == 'swamp') {
-			if (zone == 'fruit_trees') { MESH.maxfruits += zonval*(3+MESH.botanic); }
-			else if (zone == 'mountain') { MESH.maxfruits += zonval*MESH.botanic; }
-			else { MESH.maxfruits += zonval*(2+MESH.botanic); }
+			if (zone == 'fruit_trees') { MESH.maxfruits += zonval * (3 + MESH.botanic); }
+			else if (zone == 'mountain') { MESH.maxfruits += zonval * (1 + MESH.botanic); }
+			else { MESH.maxfruits += zonval * (2 + MESH.botanic); }
 		}
 		// Group death
 		if (zone == 'volcanic_activity') {
@@ -418,8 +400,8 @@ MESH.calcs = function() {
 		// Accident
 		if (zone == 'insect' || zone == 'predator' || zone == 'ruminant' || zone == 'hot' ||
 			zone == 'cold' || zone == 'sismic_activity' || zone == 'cave' || zone == 'mountain' || zone == 'ruins') {
-			MESH.accident[0] += 3*zonval;
-			MESH.accident[1] += 5*zonval;
+			MESH.accident[0] += 3 * zonval;
+			MESH.accident[1] += 5 * zonval;
 		}
 		// Fight
 		if ((zone == 'insect' || zone == 'intelligent' || zone == 'mankarog' || zone == 'predator' || zone == 'ruminant' ||
@@ -430,7 +412,7 @@ MESH.calcs = function() {
 		}
 		// Fatigue
 		if (zone == 'hot' || zone == 'cold' || zone == 'strong_wind' || zone == 'desert' || zone == 'mountain' || zone == 'swamp') {
-			MESH.fatigue += 2*zonval;
+			MESH.fatigue += 2 * zonval;
 		}
 		// Mush trap
 		if (zone == 'cristal_field') {
@@ -459,18 +441,10 @@ MESH.calcs = function() {
 	}
 	MESH.fight.sort(function(a, b) { return b-a; });
 
-	$('.MESH-heroes').each(function() {
-		var $herosel = $(this).find('select[name^="hero"]');
-		MESH.group.push($herosel.val().replace('_', ' ').capitalize());
-		MESH.bag0.push($(this).find('select[name^="item0"]').val());
-		MESH.bag1.push($(this).find('select[name^="item1"]').val());
-		MESH.bag2.push($(this).find('select[name^="item2"]').val());
-	});
 };	// END FUNCTION - MESH.calcs
 
 MESH.textarea = function() {
 	MESH.calcs();
-//	MESH.initLang();
 	var text = i18next.t('txt.title', {p_name: MESH.pname});
 	text += i18next.t('txt.info', {p_fuel: MESH.pfuel, p_dir: MESH.pdirection, zones: MESH.Totzones});
 	text += '\n' + MESH.zones.join(', ');
@@ -497,12 +471,12 @@ MESH.textarea = function() {
 	text += MESH.maxsteaks ? i18next.t('txt.steak', { count: MESH.maxsteaks }) : '';
 	text += MESH.maxfruits ? i18next.t('txt.fruit', { count: MESH.maxfruits }) : '';
 
-	text += i18next.t('txt.team', { strength: 1 });
+	text += MESH.fightzones ? i18next.t('txt.team', { count: MESH.strength }) : '';
 
 	text += i18next.t('txt.risks');
 	text += MESH.groupdeath ? i18next.t('txt.groupdeath', { count: MESH.groupdeath }) : '';
 	text += MESH.death ? i18next.t('txt.death', { count: MESH.death }) : '';
-//	text += MESH.health ? i18next.t('txt.health', { maxfuel: MESH.health }) : '';
+//	text += MESH.health ? i18next.t('txt.health', { health: MESH.health }) : '';
 	text += MESH.accident[0] ? i18next.t('txt.accident', { accident1: MESH.accident[0], accident2: MESH.accident[1] }) : '';
 	text += MESH.fatigue ? i18next.t('txt.fatigue', { fatigue: MESH.fatigue }) : '';
 	text += MESH.fightzones ? i18next.t('txt.fight', { count: MESH.fightzones, damage: MESH.fight[0] }) : '';
@@ -518,14 +492,12 @@ MESH.textarea = function() {
 
 MESH.generatext = function(addTo) {
 	var divbutt = $('<div>').addClass('divbutt MESHfloat').appendTo(addTo);
-	// TODO: Multilingual title & description
 	MESH.MakeButton('<img src="http://data.twinoid.com/img/icons/edit.png" style="vertical-align: -20%"> ' + i18next.t('button.gen_txt'), null, null,
 			i18next.t('button.gen_title'), i18next.t('button.gen_desc'))
 		.appendTo(divbutt).find("a").on("mousedown", function(){
 			MESH.textarea();
 		});
 	divbutt = $('<div>').addClass('divbutt MESHfloat').appendTo(addTo);
-	// TODO: Multilingual title & description
 	MESH.MakeButton('<img src="http://data.twinoid.com/img/icons/done.png" style="vertical-align: -20%"> ' + i18next.t('button.cpy_txt'), null, null,
 			i18next.t('button.cpy_title'), i18next.t('button.cpy_desc'))
 		.appendTo(divbutt).find("a").on("mousedown", function(){
@@ -533,7 +505,6 @@ MESH.generatext = function(addTo) {
 			document.execCommand('copy');
 		});
 	divbutt = $('<div>').addClass('divbutt MESHfloat').appendTo(addTo);
-	// TODO: Multilingual title & description
 	MESH.MakeButton('<img src="http://data.twinoid.com/img/icons/refresh.png" style="vertical-align: -20%"> ' + i18next.t('button.rst_txt'), null, null,
 			i18next.t('button.rst_title'), i18next.t('button.rst_desc'))
 		.appendTo(divbutt).find("a").on("mousedown", function(){
@@ -612,7 +583,7 @@ MESH.css = function() {
 			border: 1px solid rgb(10,40,80); \
 			padding: 2px; \
 			margin: 5px auto; \
-			font-size: 10px; \
+			font-size: 12px; \
 			color: rgb(10,40,80); \
 			opacity: 0.6; \
 			resize: none; \
@@ -709,18 +680,24 @@ MESH.css = function() {
 			text-transform: lowercase; \
 			font-size: 10px; \
 		} \
+		#MESH { \
+			font-size: 10pt; \
+		} \
+		#tooltip { z-index: 1500 !important; } \
 	").appendTo("head");
 };	// END FUNCTION - MESH.css
 
 MESH.init = function() {
 	MESH.initLang();
     MESH.varsinit();
+	MESH.initWindow();
 	MESH.css();
 	MESH.MakeButton('<img src="' + MESH.icon + '" style="vertical-align: -20%"> ' + MESH.name, null, null, i18next.t('button.title'),
 			i18next.t('button.desc'))
-		.insertAfter('#updatebtn').find("a").on("mousedown", function(){
-			MESH.OpenWindow();	// TODO: MESH.OpenWindow();
-				});
+		.insertBefore( ($('#updatebtn').length) ? '#updatebtn' : '#cdActionList')
+			.find("a").on("mousedown", function(){
+			$('#MESH').dialog("open");
+		});
 };	// END FUNCTION - MESH.init
 
 MESH.init();
